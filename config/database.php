f9f9f67bd9757229d0b89ad5cf3383f2e3ef3f8c
danpantaleon01-oss <danpantaleon01@gmail.com>
2026-05-01 22:39:09 +0800

    Add files via upload

---
+++ config/database.php
@@ -0,0 +1,9 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'campus_events');
+define('BASE_URL', 'http://localhost/SDWADPROJECT');
+define('UPLOAD_DIR', __DIR__ . '/assets/images/uploads/');
+define('UPLOAD_URL', BASE_URL . '/assets/images/uploads/');
+?>
